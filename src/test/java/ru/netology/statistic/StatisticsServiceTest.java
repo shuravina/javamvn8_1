c277d72de5ef8e3ba7f5b61df4a021f579c13105
Galina Shuravina <shuravinags@mail.ru>
2022-07-03 13:42:59 +0700

    coverage 100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -20,4 +20,18 @@ public class StatisticsServiceTest {
         assertEquals(expected, actual);
         System.out.println(actual);
     }
+
+    @Test
+    void findMaxIfNotFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 15, 11, 12};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
 }
